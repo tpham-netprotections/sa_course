2d08bad8c05c083cc731c395077d1a3970af4722
Pham Thuc Hung <tpham@netprotections.co.jp>
2018-12-11 10:19:11 +0900

    tsutsumi 納品

---
+++ Tsutsumi/netprotections/dto/UserListDto.java
@@ -0,0 +1,42 @@
+package jp.co.netprotections.dto;
+
+import java.util.List;
+
+public class UserListDto {
+	private List<UserDto> members;
+	private int login_status;
+	/**
+	 *login_status
+	 *0:logout
+	 *1:login
+	 */
+
+	/**
+	 * @return login_status
+	 */
+	public int getLogin_status() {
+		return login_status;
+	}
+
+	/**
+	 * @param login_status セットする login_status
+	 */
+	public void setLogin_status(int login_status) {
+		this.login_status = login_status;
+	}
+
+	/**
+	 * @return members
+	 */
+	public List<UserDto> getMembers() {
+		return members;
+	}
+
+	/**
+	 * @param members セットする members
+	 */
+	public void setMembers(List<UserDto> members) {
+		this.members = members;
+	}
+
+}
